f66f0716fc96e6d669d142a3b1a6b8df7ad1cddf
Ali Atakan Akman <atakan@192.168.1.11>
2025-02-21 20:09:14 +0300

    done project installation

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
+      <div className="max-w-sm rounded-2xl shadow-lg bg-white p-6 text-center">
+        <h2 className="text-2xl font-semibold text-gray-800">Tailwind Test</h2>
+        <p className="text-gray-600 mt-2">
+          Tailwind CSS başarılı bir şekilde çalışıyor! 🎉
+        </p>
+        <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition">
+          Butona Tıkla 🚀
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
